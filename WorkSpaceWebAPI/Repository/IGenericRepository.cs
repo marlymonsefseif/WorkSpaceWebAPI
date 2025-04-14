1146d0e239488dcca2a2373b786f357c1080d99c
MarlyMonsef <marlymonsef21@gmail.com>
2025-04-15 00:12:31 +0200

    Add Repo

---
+++ WorkSpaceWebAPI/Repository/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace WorkSpaceWebAPI.Repository
+{
+    public interface IGenericRepository<T>
+    {
+        List<T> GetAll();
+        T GetById(int id);
+        void Insert(T entity);
+        void Update(T entity);
+        void DeleteById(int id);
+        void Delete(T entity);
+        void Save();
+    }
+}
