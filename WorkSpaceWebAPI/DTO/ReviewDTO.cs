0439698a1d008dbba2db0d4187023627d621c271
MarlyMonsef <marlymonsef21@gmail.com>
2025-05-22 22:33:56 +0300

    review

--- WorkSpaceWebAPI/DTO/ReviewDTO.cs
+++ WorkSpaceWebAPI/DTO/ReviewDTO.cs
@@ -7,21 +7,14 @@ namespace WorkSpaceWebAPI.DTO
         public int? Id { get; set; }
         [Required]
         public int UserId { get; set; }
+        public int RoomId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
-        [Required]
-        public int RoomId { get; set; }
-
-        [Required]
         [Range(1, 5)]
         public int Rating { get; set; }
         public string? Comment { get; set; }
-        public int? UserId { get; set; }
-        public int? RoomId { get; set; }
         public DateTime? CreatedAt { get; set; }
-        public string? UserName { get; set; }
-        public string? RoomName { get; set; }
 
     }
 }
